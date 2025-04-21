e4c3d1e1c3eb86e1f6e6593ac133f4a4d579300d
wocheon <wocheon07@gmail.com>
2025-04-21 18:01:42 +0900

    202504211801

--- variables.tf
+++ variables.tf
@@ -3,57 +3,40 @@ variable "aws_region" {
   type        = string
 }
 
-variable "ami" {
+
+variable "aws_eip_allocation_id" {
   description = "EC2 인스턴스의 AMI ID"
   type        = string
+  default = null
 }
 
-variable "instance_type" {
-  description = "EC2 인스턴스 타입"
-  type        = string
-}
 
-variable "key_name" {
-  description = "EC2 인스턴스에 접속할 SSH 키 이름"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "EC2 인스턴스를 배치할 서브넷 ID"
-  type        = string
-}
-
-variable "security_group_ids" {
-  description = "EC2 인스턴스에 적용할 보안 그룹 ID 목록"
-  type        = list(string)
-}
-
-variable "associate_public_ip" {
-  description = "퍼블릭 IP 할당 여부"
+variable "aws_eip_vpc" {
+  description = "Whether to allocate the address for use with instances in a VPC"
   type        = bool
+  default     = true
 }
 
-variable "root_volume_size" {
-  description = "루트 볼륨 크기 (GB)"
-  type        = number
-}
-
-variable "root_volume_type" {
-  description = "루트 볼륨 타입 (gp2, gp3, io1 등)"
+variable "aws_eip_name" {
+  description = "Name tag for the EIP"
   type        = string
+  default     = "my-eip"
 }
 
-variable "ebs_volumes" {
-  description = "추가 EBS 볼륨 리스트"
-  type = list(object({
-    device_name = string
-    volume_size = number
-    volume_type = string
-  }))
-  default = []
-}
-
-variable "instance_name" {
-  description = "EC2 인스턴스 이름"
+variable "aws_eip_network_border_group" {
+  description = "Set the location from which the EIP will be advertised"
   type        = string
+  default     = null
+}
+
+variable "aws_eip_public_ipv4_pool" {
+  description = "EC2 IPv4 address pool identifier or amazon"
+  type        = string
+  default     = null
+}
+
+variable "aws_eip_tags" {
+  description = "Additional tags to associate with the EIP"
+  type        = map(string)
+  default     = {}
 }
