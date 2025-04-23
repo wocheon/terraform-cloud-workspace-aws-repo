4112b0ea7f8a48b194cfd757b5594899677fb275
wocheon <wocheon07@gmail.com>
2025-04-23 14:43:57 +0900

    202504231443

--- variables.tf
+++ variables.tf
@@ -55,7 +55,7 @@ variable "aws_s3_index_document" {
 variable "aws_s3_error_document" {
   type        = string
   description = "Error document (e.g., error.html)"
-  default     = error.html
+  default     = "error.html"
 }
 
 variable "aws_s3_redirect_routing_rules" {
