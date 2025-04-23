d908311d957cb13d473d694e13f59e16e38a2d30
wocheon <wocheon07@gmail.com>
2025-04-23 15:35:03 +0900

    202504231535

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ provider "aws" {
 # S3 버킷 생성
 module "s3_bucket" {
   source  = "app.terraform.io/terraform_cloud_wocheon/aws-module-registry/aws//modules/aws_s3_bucket"
-  version = "1.0.0"
+  version = "1.0.2"
   aws_s3_bucket               = var.aws_s3_bucket  
   aws_s3_force_destroy        = var.aws_s3_force_destroy
   aws_s3_object_lock_enabled  = var.aws_s3_object_lock_enabled
@@ -34,7 +34,7 @@ module "s3_bucket" {
 # 버전 관리 활성화
 module "s3_versioning" {
   source  = "app.terraform.io/terraform_cloud_wocheon/aws-module-registry/aws//modules/aws_s3_versioning"
-  version = "1.0.0"
+  version = "1.0.2"
   aws_s3_bucket             = module.s3_bucket.bucket_id
   aws_s3_version_enabled    = var.aws_s3_version_enabled 
   aws_s3_version_mfa_delete = var.aws_s3_version_mfa_delete
@@ -43,12 +43,35 @@ module "s3_versioning" {
 # 웹사이트 호스팅 설정
 module "s3_website" {
   source  = "app.terraform.io/terraform_cloud_wocheon/aws-module-registry/aws//modules/aws_s3_website"
-  version = "1.0.0"
+  version = "1.0.2"
   aws_s3_bucket                 = module.s3_bucket.bucket_id
   aws_s3_index_document         = var.aws_s3_index_document
   aws_s3_error_document         = var.aws_s3_error_document
   aws_s3_redirect_routing_rules = var.aws_s3_redirect_routing_rules
 }
+
+# Access Point 설정
+module "s3_access_point" {
+  source  = "app.terraform.io/terraform_cloud_wocheon/aws-module-registry/aws//modules/aws_s3_access_point"
+  version = "1.0.2"
+  aws_s3_bucket                 = module.s3_bucket.bucket_id
+  aws_s3_access_point_name      = var.aws_s3_access_point_name  
+  aws_s3_access_point_policy    = var.aws_s3_access_point_policy
+  aws_s3_access_point_vpc_id    = var.aws_s3_access_point_vpc_id
+  aws_s3_access_point_tags      = var.aws_s3_access_point_tags
+}
+
+# Public Accees 설정 
+module "s3_account_public_access_block" {
+  source  = "app.terraform.io/terraform_cloud_wocheon/aws-module-registry/aws//modules/aws_s3_account_public_access_block"
+  version = "1.0.2"
+  aws_s3_bucket                       = module.s3_bucket.bucket_id
+  aws_s3_apab_block_public_acls       = var.aws_s3_apab_block_public_acls
+  aws_s3_apab_block_public_policy     = var.aws_s3_apab_block_public_policy
+  aws_s3_apab_ignore_public_acls      = var.aws_s3_apab_ignore_public_acls
+  aws_s3_apab_restrict_public_buckets = var.aws_s3_apab_restrict_public_buckets
+}
+
 
 output "bucket_id" {
   value = module.s3_bucket.bucket_id
@@ -65,3 +88,18 @@ output "versioning_status" {
 output "website_endpoint" {
   value = module.s3_website.website_endpoint
 }
+
+
+output "access_point_arn" {
+  value = module.s3_access_point.access_point_arn
+}
+
+output "access_point_name" {
+  value = module.s3_access_point.access_point_name
+}
+
+
+output "public_access_block_status" {
+  value = module.s3_account_public_access_block.public_access_block_status
+}
+
--- variables.tf
+++ variables.tf
@@ -71,3 +71,53 @@ variable "aws_s3_redirect_routing_rules" {
   default = []
 }
 
+### AWS S3 Account Public Access Block Config ###
+
+variable "aws_s3_apab_block_public_acls" {
+  type        = bool
+  description = "Whether to block public ACLs"
+  default     = true
+}
+
+variable "aws_s3_apab_block_public_policy" {
+  type        = bool
+  description = "Whether to block public bucket policies"
+  default     = true
+}
+
+variable "aws_s3_apab_ignore_public_acls" {
+  type        = bool
+  description = "Whether to ignore public ACLs"
+  default     = true
+}
+
+variable "aws_s3_apab_restrict_public_buckets" {
+  type        = bool
+  description = "Whether to restrict public buckets"
+  default     = true
+}
+
+### AWS S3 Access Point Config ###
+
+variable "aws_s3_access_point_name" {
+  type        = string
+  description = "The name of the S3 Access Point"
+}
+
+variable "aws_s3_access_point_policy" {
+  type        = string
+  description = "The policy for the Access Point (JSON format)"
+  default     = "{}"  # 기본적으로 빈 정책을 설정
+}
+
+variable "aws_s3_access_point_vpc_id" {
+  type        = string
+  description = "The VPC ID to associate the Access Point"
+  default     = ""  # VPC ID가 없으면 빈 값
+}
+
+variable "aws_s3_access_point_tags" {
+  type        = map(string)
+  description = "Tags for the Access Point"
+  default     = {}
+}
